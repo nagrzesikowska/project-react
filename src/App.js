fbdab314d4a753a040a922b9152b6614563d043d
nagrzesikowska <94862333+nagrzesikowska@users.noreply.github.com>
2022-10-27 22:10:32 +0200

    added bootsrtap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="container">
         <p>Hello</p>
         Link to github
         <a
