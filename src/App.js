eb017edc82b63f52d4e765b5693d90a238d4a655
nagrzesikowska <94862333+nagrzesikowska@users.noreply.github.com>
2022-11-03 16:17:42 +0100

    container and footer added

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="container">
-        <p>Hello</p>
-        Link to github
-        <a
-          href="https://github.com/nagrzesikowska/project-react"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <p>Weather app</p>
+        <footer>
+          Coded by Natalia Grzesikowska and {""}
+          <a
+            href="https://github.com/nagrzesikowska/project-react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
